0c7d5963021e0ec1a79f2640c5c6ef222b9c2906
Carlos Moneda <moneda.psc@gmail.com>
2016-10-28 21:45:25 -0500

    menu mobile v6

--- sites/all/themes/ander/phpmex/php-scripts.js
+++ sites/all/themes/ander/phpmex/php-scripts.js
@@ -4,19 +4,19 @@ Drupalmex JS
 
 (function ($, Drupal) {
 
+  Drupal.behaviors.movilMenu = {
+    attach: function (context, settings) {
+      $( ".mobileAreaMenu ul li.expanded" ).click(function() {
+        $(this).find("ul").addClass("activo")
+      });
+    }
+  };
+
   // Adding classes container o button
   Drupal.behaviors.messagesClass = {
     attach: function (context, settings) {
       $(".messages").addClass("container");
       $(".contact-form #edit-submit, .page-events .node-readmore a").addClass("btn btn-purple");
-    }
-  };
-
-  Drupal.behaviors.movilMenu = {
-    attach: function (context, settings) {
-      $( ".mobileAreaMenu ul li.expanded" ).click(function() {
-        alert( "Handler for .click() called." );
-      });
     }
   };
 
